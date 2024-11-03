b9f1767f3d306d6477bb5785744a96c7e5a80896
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-11-03 17:20:58 +0000

    Update SoundEffect.hx

--- source/funkin/backend/audio/SoundEffect.hx
+++ source/funkin/backend/audio/SoundEffect.hx
@@ -1,14 +1,42 @@
 package funkin.backend.audio;
 
+/**
+ *	This class is used to add sound effects to different FlxSounds.
+ *	TODO: let lua be able to interact with this.
+ *
+ *	@author YourFriendOrbl
+ */
+@:access(flixel.sound.FlxSound)
 class SoundEffect
 {
-	public static function echoEffect(sound:FlxSound)
-	{
-		var echoSound:FlxSound = new FlxSound();
-		@:privateAccess echoSound.loadEmbedded(sound._sound);
-		echoSound.volume = (sound.volume / 2);
-		echoSound.pan = -.5;
+	/**
+	 *	Sound thing idk.
+	 */
+	private static var _sound(default, null):FlxSound = new FlxSound();
 
-		new flixel.util.FlxTimer().start(.1, (tmr) -> echoSound.play());
+	/**
+	 *	Echo sound effect
+	 */
+	public static function echoEffect(sound:FlxSound):Void
+	{
+		/**
+		 *	Load the sound.
+		 */
+		_sound.loadEmbedded(sound._sound);
+
+		/**
+		 *	Set the volume by dividing sound.volume by 2.
+		 */
+		_sound.volume = (sound.volume / 2);
+
+		/**
+		 *	Set the stereo panning to -0.5.
+		 */
+		_sound.pan = -.5;
+
+		/**
+		 *	After 100 milseconds the auto effect will start.
+		 */
+		new flixel.util.FlxTimer().start(.1, (tmr) -> _sound.play());
 	}
 }
