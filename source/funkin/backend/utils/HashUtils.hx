29a024e74456035cc11093fd28bb70b9d1f38aa0
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-12-09 23:40:39 +0000

    More hash types, i think...

--- source/funkin/backend/utils/HashUtils.hx
+++ source/funkin/backend/utils/HashUtils.hx
@@ -1,31 +1,38 @@
 package funkin.backend.utils;
 
-import haxe.crypto.Sha1;
-import haxe.crypto.Md5;
+import haxe.crypto.*;
 
 enum HashType
 {
+	// fr
 	MD5;
+	// sha idk?/?
 	SHA1;
+	SHA224;
+	SHA256;
+	// lua idfk
 	NONE;
 }
 
 class HashUtils
 {
-    
 	public static function hash(txt:String, type:HashType = MD5)
-        {
-            switch (type)
-            {
-                case MD5:
-                    return Md5.encode(txt);
-                case SHA1:
-                    return Sha1.encode(txt);
-                default:
-                    throw "Unsupported hash type: " + type;
-            }
-        }
-        
+	{
+		switch (type)
+		{
+			case MD5:
+				return Md5.encode(txt);
+			case SHA1:
+				return Sha1.encode(txt);
+			case SHA224:
+				return Sha224.encode(txt);
+			case SHA256:
+				return Sha256.encode(txt);
+			default:
+				throw "Unsupported hash type: " + type;
+		}
+	}
+
 	public static inline function convertHashType(hashTxt:String):HashType
 	{
 		var thingy:HashType = null;
@@ -34,6 +41,10 @@ class HashUtils
 			case 'md5':
 				thingy = MD5;
 			case 'sha1':
+				thingy = SHA1;
+			case 'sha224':
+				thingy = SHA1;
+			case 'sha256':
 				thingy = SHA1;
 			default:
 				thingy = NONE;
