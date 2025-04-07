7d8f5609bb9114c4c842eac34c264f8bb73c4f4c
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-04-07 11:08:35 +0100

    [Bug Fixes]: fixed and documented TitleState.
    i'd say its done

--- source/funkin/game/states/TitleState.hx
+++ source/funkin/game/states/TitleState.hx
@@ -6,16 +6,27 @@ import flixel.addons.transition.FlxTransitionableState;
 using funkin.backend.CoolUtil;
 
 /**
- * Title state data type.
+ * Title data struc type.
  */
 typedef TitleData =
 {
+	// title logo position.
 	logoPosition:FlxPoint,
+
+	// start text position.
 	startTextPosition:FlxPoint,
+
+	// title text colors.
+	startTextColors:Array<TitleStartColor>,
+
+	// girlfriend position.
 	gfPosition:FlxPoint,
+
+	// background sprite string.
 	backgroundSprite:String,
-	bpm:Int,
-	startTextColors:Array<TitleStartColor>
+
+	// beat per minute(?).
+	bpm:Int
 }
 
 /**
@@ -23,7 +34,10 @@ typedef TitleData =
  */
 typedef TitleStartColor =
 {
+	// color, duh
 	color:FlxColor,
+
+	// alpha, duh
 	alpha:Float
 }
 
@@ -47,6 +61,11 @@ class TitleState extends MusicBeatState
 	 */
 	var curWacky:Array<String> = [];
 
+	/**
+	 * newsground sprite.	
+	 */
+	var newsgroundSprite:FlxSprite = new FlxSprite(); // fix stupid flixel shi.
+
 	#if CHECK_FOR_UPDATES
 	/**
 	 * The update version (the newer version of this engine).
@@ -60,7 +79,7 @@ class TitleState extends MusicBeatState
 	#end
 
 	override public function create():Void
-	{	
+	{
 		curWacky = FlxG.random.getObject(introTextList);
 
 		// optim memory
@@ -99,8 +118,25 @@ class TitleState extends MusicBeatState
 		}
 		#end
 
-		// load xml data.
-		loadXMLData();
+		// fallback :3c
+		// `_data` needs to be init'd before using.
+		_data = {
+			logoPosition: FlxPoint.get(-150, -100),
+			startTextPosition: FlxPoint.get(100, 576),
+			gfPosition: FlxPoint.get(512, 40),
+			bpm: 102,
+			backgroundSprite: "",
+			startTextColors: [
+				{
+					color: 0xFFFFA033,
+					alpha: 1
+				},
+				{color: 0xFF3333CC, alpha: .64}
+			]
+		};
+
+		// load data.
+		#if TITLE_SCREEN_XML loadXMLData(); #end
 
 		persistentUpdate = persistentDraw = !initialized;
 
@@ -125,53 +161,40 @@ class TitleState extends MusicBeatState
 			Logs.prefixedTrace('Max Score: $maxS - Max Misses: $mostM', 'Info', ORANGE);
 	}
 
-		/**
-	* Has the intro been skipped.	
-	*/
+	/**
+	 * Has the intro been skipped.	
+	 */
 	var skippedIntro:Bool = false;
 
 	/**
-	* Skip intro function.	
-	*/
+	 * Skip intro function.	
+	 */
 	function skipIntro():Void
 		if (!skippedIntro)
-		{	
+		{
 			introGroup.empty();
 			remove(introGroup);
 			FlxG.camera.flash(FlxColor.WHITE, 4);
-
 			skippedIntro = true;
 		}
 
+	#if TITLE_SCREEN_XML
+	/**
+	 * Load XML data.
+	 */
 	function loadXMLData():Void
 	{
-		// fallback :3c
-		_data = {
-			logoPosition: FlxPoint.get(-150, -100),
-			startTextPosition: FlxPoint.get(100, 576),
-			gfPosition: FlxPoint.get(512, 40),
-			bpm: 102,
-			backgroundSprite: "",
-			startTextColors: [
-				{
-					color: 0xFFFFA033,
-					alpha: 1
-				},
-				{color: 0xFF3333CC, alpha: .64}
-			]
-		};
-
-		#if TITLE_SCREEN_XML
 		try
 		{
-			final xml = new Access(Xml.parse(Paths.getTextFromFile('data/config/titlescreen.xml')).firstElement());
+			final xml:Access = new Access(Xml.parse(Paths.getTextFromFile('data/config/titlescreen.xml')).firstElement());
 			if (xml != null)
 			{
 				// Title logo position xml data.
 				final logoPos = FlxPoint.get(-150, -100);
 				final node:Access = xml.node.logo;
 				if (node.name != null)
-					logoPos.set(Std.parseFloat(node.has.x ? node.att.x : null).getDefault(-150), Std.parseFloat(node.has.y ? node.att.y : null).getDefault(-100));
+					logoPos.set(Std.parseFloat(node.has.x ? node.att.x : null).getDefault(-150),
+						Std.parseFloat(node.has.y ? node.att.y : null).getDefault(-100));
 				_data.logoPosition = logoPos;
 
 				// Start text xml data.
@@ -188,7 +211,7 @@ class TitleState extends MusicBeatState
 						color: colorNode.has.color ? colorNode.att.color.colorFromString() : FlxColor.WHITE,
 						alpha: Std.parseFloat(colorNode.has.alpha ? colorNode.att.alpha : null).getDefault(1.0)
 					}
-	
+
 				final colorStartData = node.has.colorStart ? extractTitleColor(node.node.colorStart) : {color: 0xFF33FFFF, alpha: 1.0};
 				final colorEndData = node.has.colorEnd ? extractTitleColor(node.node.colorEnd) : {color: 0xFF3333CC, alpha: .64};
 				_data.startTextColors = [colorStartData, colorEndData];
@@ -204,41 +227,45 @@ class TitleState extends MusicBeatState
 				_data.bpm = Std.parseInt(xml.has.bpm ? xml.att.bpm : null).getDefault(102);
 				_data.backgroundSprite = xml.has.backgroundSprite ? xml.att.backgroundSprite : "";
 			}
-		} catch (error:Dynamic)
+		}
+		catch (error:Dynamic)
 			Logs.prefixedTrace(error, 'Credits State', RED);
-		#end
 	}
+	#end
 
 	/**
-	* Title intro text group.	
-	*/
+	 * Title intro text group.	
+	 */
 	var introGroup:TitleIntroGroup;
 
 	/**
-	* Friday Night Funkin' Logo.	
-	*/
+	 * Friday Night Funkin' Logo.	
+	 */
 	var logo:FlxSprite;
 
 	/**
-	* Girlfriend dance.	
-	*/
+	 * Girlfriend dance.	
+	 */
 	var gfDance:FlxSprite;
 
 	/**
-	* Should Girlfriend dance left.	
-	*/
+	 * Should Girlfriend dance left.	
+	 */
 	var danceLeft:Bool = false;
 
 	/**
-	* Enter text.	
-	*/
+	 * Enter text.	
+	 */
 	var titleText:FlxSprite;
 
 	/**
-	* Color swap shader.
-	*/
+	 * Color swap shader.
+	 */
 	var swagShader:ColorSwap = null;
 
+	/**
+	 * Start the intro.	
+	 */
 	function startIntro():Void
 	{
 		if (!initialized)
@@ -248,6 +275,7 @@ class TitleState extends MusicBeatState
 		Conductor.bpm = _data.bpm;
 		persistentUpdate = true;
 
+		// background
 		final bg:FlxSprite = new FlxSprite();
 		if (_data.backgroundSprite != null && _data.backgroundSprite.length > 0 && _data.backgroundSprite != "none")
 			bg.loadGraphic(Paths.image(_data.backgroundSprite));
@@ -255,6 +283,7 @@ class TitleState extends MusicBeatState
 			bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
+		// logo.
 		logo = new FlxSprite(_data.logoPosition.x, _data.logoPosition.y);
 		logo.frames = Paths.getSparrowAtlas('logoBumpin');
 		logo.antialiasing = ClientPrefs.data.antialiasing;
@@ -262,8 +291,10 @@ class TitleState extends MusicBeatState
 		logo.animation.play('bump');
 		logo.updateHitbox();
 
+		// color swap shader.
 		swagShader = new ColorSwap();
 
+		// girlfriend dancing anim.
 		gfDance = new FlxSprite(_data.gfPosition.x, _data.gfPosition.y);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
@@ -272,32 +303,22 @@ class TitleState extends MusicBeatState
 		gfDance.shader = swagShader.shader;
 		add(gfDance);
 
+		// logo shader.
 		logo.shader = swagShader.shader;
 		add(logo);
 
+		final animFrames:Array<flixel.graphics.frames.FlxFrame> = [];
 		titleText = new FlxSprite((_data.startTextPosition.x + 30), _data.startTextPosition.y);
 		titleText.frames = Paths.getSparrowAtlas('titleEnter');
-
-		final animFrames:Array<flixel.graphics.frames.FlxFrame> = [];
 		@:privateAccess {
 			titleText.animation.findByPrefix(animFrames, "ENTER IDLE");
 			titleText.animation.findByPrefix(animFrames, "ENTER FREEZE");
 		}
 
-		if (animFrames.length > 0)
-		{
-			newTitle = true;
-
-			titleText.animation.addByPrefix('idle', "ENTER IDLE", 24);
-			titleText.animation.addByPrefix('press', ClientPrefs.data.flashing ? "ENTER PRESSED" : "ENTER FREEZE", 24);
-		}
-		else
-		{
-			newTitle = false;
-
-			titleText.animation.addByPrefix('idle', "Press Enter to Begin", 24);
-			titleText.animation.addByPrefix('press', "ENTER PRESSED", 24);
-		}
+		// anim handler
+		newTitle = (animFrames.length > 0);
+		titleText.animation.addByPrefix('idle', animFrames.length > 0 ? "ENTER IDLE" : "Press Enter to Begin", 24);
+		titleText.animation.addByPrefix('press', animFrames.length > 0 ? (ClientPrefs.data.flashing ? "ENTER PRESSED" : "ENTER FREEZE") : "ENTER PRESSED", 24);
 
 		titleText.antialiasing = ClientPrefs.data.antialiasing;
 		titleText.animation.play('idle');
@@ -306,29 +327,42 @@ class TitleState extends MusicBeatState
 
 		add(introGroup = new TitleIntroGroup());
 
+		// newsground sprite stuff.
+		newsgroundSprite = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('newgrounds_logo'));
+		newsgroundSprite.visible = false;
+		newsgroundSprite.setGraphicSize(Std.int(newsgroundSprite.width * 0.8));
+		newsgroundSprite.updateHitbox();
+		newsgroundSprite.screenCenter(X);
+		newsgroundSprite.antialiasing = ClientPrefs.data.antialiasing;
+		introGroup.add(newsgroundSprite);
+
 		initialized ? skipIntro() : initialized = true;
 	}
 
 	/**
-	* Intro text list.	
-	*/
+	 * Intro text list.	
+	 */
 	var introTextList(get, never):Array<Array<String>>;
+
 	@:dox(hide) function get_introTextList():Array<Array<String>>
-		return [for (i in #if MODS_ALLOWED Mods.mergeAllTextsNamed('data/introText.txt') #else Assets.getText(Paths.txt('introText')).split('\n') #end) i.split('--')];
+		return [
+			for (i in #if MODS_ALLOWED Mods.mergeAllTextsNamed('data/introText.txt') #else Assets.getText(Paths.txt('introText'))
+				.split('\n') #end) i.split('--')
+		];
 
 	/**
-	* Is the state transitioning.	
-	*/
+	 * Is the state transitioning.	
+	 */
 	var transitioning:Bool = false;
 
 	/**
-	*	New title???
-	*/
+	 *	New title???
+	 */
 	var newTitle:Bool = false;
 
 	/**
-	* The title timer.	
-	*/
+	 * The title timer.	
+	 */
 	var titleTimer:Float = 0;
 
 	@:dox(hide) override function update(elapsed:Float):Void
@@ -341,9 +375,13 @@ class TitleState extends MusicBeatState
 
 		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER || controls.ACCEPT;
 
-		#if ASTRO_WATERMARKS if (FlxG.keys.justPressed.SEVEN) MusicBeatState.switchState("VeryFuniState", [new TitleState()]); /* YES, FINALLY IT WORKS!!?!?!?!? */ #end
-		#if mobile for (touch in FlxG.touches.list) if (touch.justPressed) pressedEnter = true; #end
-
+		#if ASTRO_WATERMARKS if (FlxG.keys.justPressed.SEVEN)
+			MusicBeatState.switchState("VeryFuniState", [new TitleState()]); /* YES, FINALLY IT WORKS!!?!?!?!? */ #end
+		#if mobile for (touch in FlxG.touches.list)
+			if (touch.justPressed)
+				pressedEnter = true; #end
+		
+		// game controller support.
 		final gamepad:flixel.input.gamepad.FlxGamepad = FlxG.gamepads.lastActive;
 		if (gamepad != null)
 		{
@@ -354,8 +392,7 @@ class TitleState extends MusicBeatState
 		if (newTitle)
 		{
 			titleTimer += MathsAddon.boundTo(elapsed, 0, 1);
-			if (titleTimer > 2)
-				titleTimer -= 2;
+			if (titleTimer > 2) titleTimer -= 2;
 		}
 
 		if (initialized && !transitioning && skippedIntro)
@@ -363,8 +400,7 @@ class TitleState extends MusicBeatState
 			if (newTitle && !pressedEnter)
 			{
 				var timer:Float = titleTimer;
-				if (timer >= 1)
-					timer = (-timer) + 2;
+				if (timer >= 1) timer = (-timer) + 2;
 
 				timer = FlxEase.quadInOut(timer);
 
@@ -387,14 +423,17 @@ class TitleState extends MusicBeatState
 
 				new FlxTimer().start(1, function(tmr:FlxTimer):Void
 				{
-					MusicBeatState.switchState(#if CHECK_FOR_UPDATES _mustUpdate ? new funkin.game.states.OutdatedState() : #end new funkin.game.states.MainMenuState());
+					MusicBeatState.switchState(#if CHECK_FOR_UPDATES _mustUpdate ? new funkin.game.states.OutdatedState() : #end
+						new funkin.game.states.MainMenuState());
 					closedState = true;
 				});
 			}
 		}
 
-		if (initialized && pressedEnter && !skippedIntro) skipIntro();
+		if (initialized && pressedEnter && !skippedIntro)
+			skipIntro();
 
+		// hue control.
 		if (swagShader != null)
 		{
 			if (controls.UI_LEFT) swagShader.hue -= elapsed * 0.1;
@@ -405,8 +444,8 @@ class TitleState extends MusicBeatState
 	}
 
 	/**
-	* Has the state been closed.	
-	*/
+	 * Has the state been closed.	
+	 */
 	public static var closedState:Bool = false;
 
 	/**
@@ -418,48 +457,36 @@ class TitleState extends MusicBeatState
 	{
 		super.beatHit();
 
-		if (logo != null)
-			logo.animation.play('bump', true);
+		// logo bump animation, maybe use flxtweens instead?
+		if (logo != null) logo.animation.play('bump', true);
 
+		// girlfriend dancin'
 		if (gfDance != null)
 		{
 			danceLeft = !danceLeft;
-			if (danceLeft)
-				gfDance.animation.play('danceRight');
-			else
-				gfDance.animation.play('danceLeft');
+			gfDance.animation.play(danceLeft ? 'danceRight' : 'danceLeft');
 		}
 
-		// intro.
-		var newsgroundSprite:FlxSprite = new FlxSprite();// fix stupid flixel shi.
+		// intro beats.
 		if (!closedState)
 		{
 			// basically controls the fnf intro
 			sickBeats++;
-			switch (sickBeats)//TODO: include this inside the .xml file.
+			switch (sickBeats) // TODO: include this inside the .xml file.
 			{
 				case 1:
 					FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 					FlxG.sound.music.fadeIn(4, 0, 0.7);
-					
-					// newsground sprite stuff.
-					newsgroundSprite = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('newgrounds_logo'));
-					newsgroundSprite.visible = false;
-					newsgroundSprite.setGraphicSize(Std.int(newsgroundSprite.width * 0.8));
-					newsgroundSprite.updateHitbox();
-					newsgroundSprite.screenCenter(X);
-					newsgroundSprite.antialiasing = ClientPrefs.data.antialiasing;
-					introGroup.add(newsgroundSprite);
 				case 2:
-					introGroup.create(['Astro Engine by'], FlxPoint.get(0,40));
+					introGroup.create(['Astro Engine by'], FlxPoint.get(0, 40));
 				case 4:
-					introGroup.include('YourFriendOrbl', FlxPoint.get(0,40));
+					introGroup.include('YourFriendOrbl', FlxPoint.get(0, 40));
 				case 5:
 					introGroup.empty();
 				case 6:
-					introGroup.create(['Not associated', 'with'], FlxPoint.get(0,-40));
+					introGroup.create(['Not associated', 'with'], FlxPoint.get(0, -40));
 				case 8:
-					introGroup.include('newgrounds', FlxPoint.get(0,-40));
+					introGroup.include('newgrounds', FlxPoint.get(0, -40));
 					newsgroundSprite.visible = true;
 				case 9:
 					introGroup.empty();
