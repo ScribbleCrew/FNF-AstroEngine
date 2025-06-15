5980f58e9a91a0b043282bddd5c7ed05968dcbe1
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-06-15 12:03:35 +0100

    Create ExampleShader.hx

---
+++ source/funkin/game/objects/shaders/ExampleShader.hx
@@ -0,0 +1,15 @@
+package funkin.game.objects.shaders;
+
+import flixel.system.FlxAssets.FlxShader;
+
+class ExampleShader extends ShaderBackend
+{
+	@:glFragmentSource('')
+	
+	public function new()
+	{
+		super();
+	}
+
+	override function update(elapsed:Float) {}
+}
