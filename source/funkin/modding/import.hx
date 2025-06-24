608c163e12fde70176ebcefdd1e4731185c557e5
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-06-24 17:14:37 +0100

    import fixes

--- source/funkin/modding/import.hx
+++ source/funkin/modding/import.hx
@@ -4,14 +4,17 @@ package funkin.modding;
 // BASE SCRIPTING STUFF
 import funkin.modding.*;
 import funkin.modding.interfaces.*;
-import funkin.modding.FunkinLua;
 import funkin.modding.ScriptUtil.FunctionFlag;
 #if HSCRIPT_ALLOWED
-// HSCRIPT // RULESCRIPT
+// HSCRIPT
 import rulescript.*;
 import rulescript.parsers.*;
 import rulescript.types.Typedefs;
 import rulescript.types.ScriptedTypeUtil;
 import rulescript.scriptedClass.RuleScriptedClassUtil;
+#end
+#if LUA_ALLOWED
+// LUA
+import funkin.modding.FunkinLua;
 #end
 #end
