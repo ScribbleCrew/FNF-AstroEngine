0471f18fd55d7b2497cd412675b65f14f19b08d1
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-07-17 21:55:49 +0100

    Update AnimationController.hx

--- source/funkin/backend/animation/AnimationController.hx
+++ source/funkin/backend/animation/AnimationController.hx
@@ -3,15 +3,12 @@ package funkin.backend.animation;
 // add custom anim offsets
 class AnimationController extends flixel.animation.FlxAnimationController
 {
-	/**
-	* Follow the global speed.	
-	*/
 	public var followGlobalSpeed:Bool = true;
-	
-	@:dox(hide) override function update(elapsed:Float):Void
+
+	@:dox(hide) override function update(v1:Float):Void
 	{
 		if (_curAnim != null)
-			_curAnim.update(elapsed * (timeScale * (followGlobalSpeed ? FlxG.animationTimeScale : 0)));
+			_curAnim.update(v1 * (timeScale * (followGlobalSpeed ? FlxG.animationTimeScale : 0)));
 		else if (_prerotated != null)
 			_prerotated.angle = _sprite.angle;
 	}
